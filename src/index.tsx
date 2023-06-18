f96df46040d40578106f805d168f654c47d9026e
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2023-06-18 12:18:34 +0900

    first-commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbf4MAtqjIzxx4i7YqKJEJ_FsPA_sQOO4",
+  authDomain: "performance-management-8a444.firebaseapp.com",
+  projectId: "performance-management-8a444",
+  storageBucket: "performance-management-8a444.appspot.com",
+  messagingSenderId: "814196915682",
+  appId: "1:814196915682:web:50ae84119e6c3f28c28dbb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
