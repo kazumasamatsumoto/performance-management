2a6d1e4b73bf4f9d1e0e22eea71c86cfcee46e5d
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2023-06-20 11:56:20 +0900

    Report

--- src/ReportPage.tsx
+++ src/ReportPage.tsx
@@ -9,6 +9,9 @@ import {
   getDocs,
   query,
   orderBy,
+  doc,
+  deleteDoc,
+  updateDoc,
 } from "firebase/firestore";
 import { Link } from "react-router-dom";
 
@@ -17,7 +20,11 @@ const ReportPage = () => {
   const [date, setDate] = useState("");
   const [records, setRecords] = useState([{ project: "", hours: "" }]);
   const [pastRecords, setPastRecords] = useState<any[]>([]);
-  const [projects, setProjects] = useState<any[]>([]); // 追加
+  const [projects, setProjects] = useState<any[]>([]);
+  const [editingRecord, setEditingRecord] = useState<{
+    id: string | null;
+    data: any | null;
+  }>({ id: null, data: null });
 
   const handleInputChange = (index: any, event: any) => {
     const values = [...records];
@@ -33,6 +40,24 @@ const ReportPage = () => {
     setRecords([...records, { project: "", hours: "" }]);
   };
 
+  const handleDelete = async (recordId: string) => {
+    if (user) {
+      await deleteDoc(doc(db, "users", user.uid, "records", recordId));
+      fetchPastRecords();
+    }
+  };
+
+  const handleEdit = async () => {
+    if (user && editingRecord.id && editingRecord.data) {
+      await updateDoc(
+        doc(db, "users", user.uid, "records", editingRecord.id),
+        editingRecord.data
+      );
+      fetchPastRecords();
+      setEditingRecord({ id: null, data: null });
+    }
+  };
+
   const fetchPastRecords = useCallback(async () => {
     if (user) {
       const q = query(
@@ -40,11 +65,12 @@ const ReportPage = () => {
         orderBy("date")
       );
       const querySnapshot = await getDocs(q);
-      setPastRecords(querySnapshot.docs.map((doc) => doc.data()));
+      setPastRecords(
+        querySnapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))
+      );
     }
   }, [user]);
 
-  // 追加：案件をフェッチする
   const fetchProjects = async () => {
     const querySnapshot = await getDocs(collection(db, "projects"));
     setProjects(
@@ -54,18 +80,14 @@ const ReportPage = () => {
 
   const handleSave = async () => {
     if (user && date) {
-      const docRef = await addDoc(
-        collection(db, "users", user.uid, "records"),
-        {
-          date,
-          records,
-        }
-      );
-      console.log(docRef)
+      await addDoc(collection(db, "users", user.uid, "records"), {
+        date,
+        records,
+      });
       fetchPastRecords();
     }
   };
-  // これを追加
+
   useEffect(() => {
     fetchProjects();
   }, []);
@@ -82,7 +104,8 @@ const ReportPage = () => {
         type="date"
         value={date}
         onChange={(e) => setDate(e.target.value)}
-        className="mb-4 p-2 border border-gray-300 rounded"
+        className="mb-4 p-2
+        border-gray-300 rounded"
       />
 
       {records.map((record, index) => (
@@ -141,26 +164,128 @@ const ReportPage = () => {
             <th className="px-4 py-2">日付</th>
             <th className="px-4 py-2">案件</th>
             <th className="px-4 py-2">時間</th>
+            <th className="px-4 py-2">操作</th>
           </tr>
         </thead>
         <tbody>
           {pastRecords.map((record, index) => (
             <React.Fragment key={index}>
-              {record.records.map((item: any, i: any) => (
+              {record.data.records.map((item: any, i: any) => (
                 <tr key={i} className="text-center border">
                   {i === 0 && (
-                    <td className="px-4 py-2" rowSpan={record.records.length}>{record.date}</td>
+                    <td
+                      className="px-4 py-2"
+                      rowSpan={record.data.records.length}
+                    >
+                      {record.data.date}
+                    </td>
                   )}
                   <td className="px-4 py-2">{item.project}</td>
                   <td className="px-4 py-2">{item.hours}時間</td>
+                  <td className="px-4 py-2">
+                    <button
+                      onClick={() => setEditingRecord(record)}
+                      className="bg-yellow-500 text-white py-1 px-2 rounded hover:bg-yellow-700"
+                    >
+                      編集
+                    </button>
+                    <button
+                      onClick={() => handleDelete(record.id)}
+                      className="bg-red-500 text-white py-1 px-2 rounded hover:bg-red-700 ml-2"
+                    >
+                      削除
+                    </button>
+                  </td>
                 </tr>
               ))}
             </React.Fragment>
           ))}
         </tbody>
       </table>
-    </div>
 
+      {editingRecord.id && (
+        <div className="p-4 mb-4 border border-gray-300 rounded">
+          <h3 className="text-lg mb-2">Record editing:</h3>
+
+          <label className="block">
+            <span className="text-gray-700">Date:</span>
+            <input
+              type="date"
+              value={editingRecord.data.date}
+              onChange={(e) =>
+                setEditingRecord((prev) => ({
+                  ...prev,
+                  data: { ...prev.data, date: e.target.value },
+                }))
+              }
+              className="w-full mt-1 p-2 mb-2 border border-gray-300 rounded"
+            />
+          </label>
+
+          <label className="block">
+            <span className="text-gray-700">Project:</span>
+            <input
+              type="text"
+              value={editingRecord.data.records[0].project}
+              onChange={(e) =>
+                setEditingRecord((prev) => ({
+                  ...prev,
+                  data: {
+                    ...prev.data,
+                    records: [
+                      {
+                        project: e.target.value,
+                        hours: prev.data.records[0].hours,
+                      },
+                    ],
+                  },
+                }))
+              }
+              className="w-full mt-1 p-2 mb-2 border border-gray-300 rounded"
+            />
+          </label>
+
+          <label className="block">
+            <span className="text-gray-700">Hours:</span>
+            <input
+              type="number"
+              step="0.5"
+              value={editingRecord.data.records[0].hours}
+              onChange={(e) =>
+                setEditingRecord((prev) => ({
+                  ...prev,
+                  data: {
+                    ...prev.data,
+                    records: [
+                      {
+                        project: prev.data.records[0].project,
+                        hours: e.target.value,
+                      },
+                    ],
+                  },
+                }))
+              }
+              className="w-full mt-1 p-2 mb-2 border border-gray-300 rounded"
+            />
+          </label>
+
+          <div className="flex space-x-2">
+            <button
+              onClick={handleEdit}
+              className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+            >
+              Update
+            </button>
+            <button
+              onClick={() => setEditingRecord({ id: null, data: null })}
+              className="w-full bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-700"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
